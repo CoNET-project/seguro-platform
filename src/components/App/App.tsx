e54b0e56b4a802ce5e8200b01d31607576aa5e4d
AquaDuck1 <79480213+AquaDuck1@users.noreply.github.com>
2021-06-29 14:21:04 -0700

    Add basic stuff

---
+++ src/components/App/App.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+    font-size: 24px;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+
+const App = () => {
+    return (
+        <StyledContainer>
+            Seguro Platform
+        </StyledContainer>
+    )
+}
+
+export default App
