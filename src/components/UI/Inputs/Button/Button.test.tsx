0e0bde992d25f66b507b462840b3694fa2479afc
AquaDuck1 <79480213+AquaDuck1@users.noreply.github.com>
2021-07-21 13:49:53 -0700

    Add specific babel-loader dependency

---
+++ src/components/UI/Inputs/Button/Button.test.tsx
@@ -0,0 +1,33 @@
+import { fireEvent, render } from '@testing-library/react'
+import Button from './Button'
+
+describe('Button', () => {
+
+    it('should render component', () => {
+        const { getByText } = render(
+            <Button
+                onClick={() => {}}
+            >
+                Click Me
+            </Button>
+        )
+
+        getByText('Click Me')
+    })
+
+    it('should execute click handler when clicked', () => {
+        const mockOnClick = jest.fn()
+
+        const { getByText } = render(
+            <Button
+                onClick={mockOnClick}
+            >
+                Click Me
+            </Button>
+        )
+
+        fireEvent.click(getByText('Click Me'))
+
+        expect(mockOnClick).toHaveBeenCalled()
+    })
+})
