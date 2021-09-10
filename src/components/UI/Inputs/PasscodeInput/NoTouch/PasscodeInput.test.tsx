73148c11e1f95f1c32d08b6b00a857faae3dcd4a
aybeeseedee <80135066+aybeeseedee@users.noreply.github.com>
2021-09-10 16:09:17 -0700

    v0.13.0

--- src/components/UI/Inputs/PasscodeInput/NoTouch/PasscodeInput.test.tsx
+++
@@ -1,8 +0,0 @@
-import {fireEvent, render} from '../../../../../utilities/testing/testing'
-
-describe('PasscodeInput', () => {
-
-    it('should pass', () => {
-        expect(1).toEqual(1)
-    })
-})
