3f119ff5cd352ef87ef56e4b9849057a52c58987
aybeeseedee <80135066+aybeeseedee@users.noreply.github.com>
2021-09-15 14:36:13 -0700

    v0.14.0

--- src/components/UI/Keypad/Key/Key.test.tsx
+++
@@ -1,8 +0,0 @@
-import { fireEvent, render } from '../../../../utilities/testing/testing'
-
-describe('Keypad Key', () => {
-
-    it('should pass', () => {
-        expect(1).toEqual(1)
-    })
-})
