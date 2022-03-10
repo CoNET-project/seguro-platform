55d444aca37911b17d8e549e22405305d5d30bb1
aybeeseedee <80135066+aybeeseedee@users.noreply.github.com>
2022-03-10 15:25:26 -0800

    Fix wallpapers in settings

--- src/components/App/Apps/Messenger/Panels/LeftPanel/Settings/Chats/Wallpaper.tsx
+++ src/components/App/Apps/Messenger/Panels/LeftPanel/Settings/Chats/Wallpaper.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Header from "../../Header";
+import {randomColor} from "../../../../../../../../utilities/utilities";
+import {useState} from "react";
 
 const StyledContainer = styled.div`
   height: 100%;
@@ -9,36 +11,69 @@ const StyledContainer = styled.div`
 const StyledContent = styled.div`
   height: calc(100% - 50px);
   width: 100%;
-  padding: 10px;
+  padding: 30px 10px;
   display: flex;
   justify-content: center;
 `
 
 const StyledWallpaperGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 100px);
-  grid-template-rows: repeat(2, 100px);
+  grid-template-columns: repeat(2, 125px);
+  grid-template-rows: repeat(2, 125px);
   grid-gap: 10px;
+`
+
+const StyledWallpaperSelection = styled.div`
+  width: 100%;
+  height: 100%;
+  content: '';
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
 
 const StyledWallpaperBox = styled.div`
   width: 100px;
   height: 100px;
   content: '';
-  background-color: red;
+  background-color: ${props => randomColor()};
   display: flex;
 `
 
+const StyledWallpaperRadio = styled.input`
+  margin-top: 10px;
+`
+
 const Wallpaper = () => {
+
+    const [selected, setSelected] = useState(1)
+
+
     return (
         <StyledContainer>
             <Header/>
             <StyledContent>
                 <StyledWallpaperGrid>
-                    <StyledWallpaperBox/>
-                    <StyledWallpaperBox/>
-                    <StyledWallpaperBox/>
-                    <StyledWallpaperBox/>
+                    <StyledWallpaperSelection>
+                        <StyledWallpaperBox/>
+                        <StyledWallpaperRadio type="radio" defaultChecked={selected == 1}
+                                              onClick={() => setSelected(1)}/>
+                    </StyledWallpaperSelection>
+                    <StyledWallpaperSelection>
+                        <StyledWallpaperBox/>
+                        <StyledWallpaperRadio type="radio" defaultChecked={selected == 2}
+                                              onClick={() => setSelected(2)}/>
+                    </StyledWallpaperSelection>
+                    <StyledWallpaperSelection>
+                        <StyledWallpaperBox/>
+                        <StyledWallpaperRadio type="radio" defaultChecked={selected == 3}
+                                              onClick={() => setSelected(3)}/>
+                    </StyledWallpaperSelection>
+                    <StyledWallpaperSelection>
+                        <StyledWallpaperBox/>
+                        <StyledWallpaperRadio type="radio" defaultChecked={selected == 4}
+                                              onClick={() => setSelected(4)}/>
+                    </StyledWallpaperSelection>
                 </StyledWallpaperGrid>
             </StyledContent>
         </StyledContainer>
